dd7a9be9ec76fcde69801abd15f0f30f9390b4d7
chen-chun-yu <adam@gapp.nthu.edu.tw>
2020-11-07 17:23:12 +0800

    add End feature

--- gen_feature_vector.py
+++ gen_feature_vector.py
@@ -12,84 +12,149 @@ def gen_feature_vec():
     '''
     feature_vec_list = []
     #Initial state, [Start, a] and [Start, h] and [Start, n] and [Start, s]
-    feature_vec_list.append({'Start_a':[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            'Start_h':[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            'Start_n':[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            'Start_s':[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]})
+    feature_vec_list.append({'Start_a_End':[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+                            'Start_h_End':[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+                            'Start_n_End':[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+                            'Start_s_End':[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]})
     
-    for dialog_len in range(2, 111, 1): #max dialog length is 110
+    for dialog_len in range(2, 4, 1): #max dialog length is 110
         print("Processing dialog len", dialog_len)
         feature_vec_dict = {}
         for pre_utt in feature_vec_list[dialog_len-2].keys():
+            pre_utt_non_End = pre_utt[:-4]
             #print(pre_utt[-1])
             #print(feature_vec_list[dialog_len-2][pre_utt])
-            if pre_utt[-1] == 'a':
+            if pre_utt_non_End[-1] == 'a':
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[4] += 1
-                feature_vec_dict[pre_utt+'_a'] = list_copy
+                list_copy[20] = 1
+                list_copy[21] = 0
+                list_copy[22] = 0
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_a_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[5] += 1
-                feature_vec_dict[pre_utt+'_h'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 1
+                list_copy[22] = 0
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_h_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[6] += 1
-                feature_vec_dict[pre_utt+'_n'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 0
+                list_copy[22] = 1
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_n_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[7] += 1
-                feature_vec_dict[pre_utt+'_s'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 0
+                list_copy[22] = 0
+                list_copy[23] = 1
+                feature_vec_dict[pre_utt_non_End+'_s_End'] = list_copy
 
-            elif pre_utt[-1] == 'h':
+            elif pre_utt_non_End[-1] == 'h':
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[8] += 1
-                feature_vec_dict[pre_utt+'_a'] = list_copy
+                list_copy[20] = 1
+                list_copy[21] = 0
+                list_copy[22] = 0
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_a_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[9] += 1
-                feature_vec_dict[pre_utt+'_h'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 1
+                list_copy[22] = 0
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_h_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[10] += 1
-                feature_vec_dict[pre_utt+'_n'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 0
+                list_copy[22] = 1
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_n_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[11] += 1
-                feature_vec_dict[pre_utt+'_s'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 0
+                list_copy[22] = 0
+                list_copy[23] = 1
+                feature_vec_dict[pre_utt_non_End+'_s_End'] = list_copy
 
-            elif pre_utt[-1] == 'n':
+            elif pre_utt_non_End[-1] == 'n':
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[12] += 1
-                feature_vec_dict[pre_utt+'_a'] = list_copy
+                list_copy[20] = 1
+                list_copy[21] = 0
+                list_copy[22] = 0
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_a_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[13] += 1
-                feature_vec_dict[pre_utt+'_h'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 1
+                list_copy[22] = 0
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_h_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[14] += 1
-                feature_vec_dict[pre_utt+'_n'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 0
+                list_copy[22] = 1
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_n_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[15] += 1
-                feature_vec_dict[pre_utt+'_s'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 0
+                list_copy[22] = 0
+                list_copy[23] = 1
+                feature_vec_dict[pre_utt_non_End+'_s_End'] = list_copy
 
-            elif pre_utt[-1] == 's':
+            elif pre_utt_non_End[-1] == 's':
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[16] += 1
-                feature_vec_dict[pre_utt+'_a'] = list_copy
+                list_copy[20] = 1
+                list_copy[21] = 0
+                list_copy[22] = 0
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_a_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[17] += 1
-                feature_vec_dict[pre_utt+'_h'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 1
+                list_copy[22] = 0
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_h_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[18] += 1
-                feature_vec_dict[pre_utt+'_n'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 0
+                list_copy[22] = 1
+                list_copy[23] = 0
+                feature_vec_dict[pre_utt_non_End+'_n_End'] = list_copy
 
                 list_copy = feature_vec_list[dialog_len-2][pre_utt].copy()
                 list_copy[19] += 1
-                feature_vec_dict[pre_utt+'_s'] = list_copy
+                list_copy[20] = 0
+                list_copy[21] = 0
+                list_copy[22] = 0
+                list_copy[23] = 1
+                feature_vec_dict[pre_utt_non_End+'_s_End'] = list_copy
 
         feature_vec_list.append(feature_vec_dict)
     '''
